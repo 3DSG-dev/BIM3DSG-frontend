fe4a994ab63350cde659940f8835b50baee39d51
ITinnovations <72745273+ITinnovations@users.noreply.github.com>
2018-06-22 14:21:23 +0000

    order schede by position, titolo
    git-tfs-id: [https://tfs.bim3dsg.polimi.it/PoliBIM/]$/BIM3DSG;C202

--- php/getListaSchede.php
+++ php/getListaSchede.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['validUser'])) {
 set_time_limit(0);
 $dbconn = pg_connect($_SESSION['dbConnectionString']) or die ('Error connecting to db');
 
-$SQL = "SELECT * FROM \"Oggetti_ListaSchede\"";
+$SQL = "SELECT * FROM \"Oggetti_ListaSchede\" ORDER BY \"Posizione\", \"Titolo\"";
 $result1 = pg_query($dbconn, $SQL) or die ("Error: $SQL");
 $myArray = array();
 while ($tmp = pg_fetch_array($result1, NULL, PGSQL_ASSOC)) {
